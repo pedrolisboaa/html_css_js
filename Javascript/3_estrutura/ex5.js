ca1f7cf43a379314d52c1683d45c94cf96df82d8
pedrolisboaa <phlisboa2000@gmail.com>
2023-07-26 15:48:08 -0300

    atualizando

---
+++ Javascript/3_estrutura/ex5.js
@@ -0,0 +1,8 @@
+let velocidade = 30
+
+
+if (velocidade > 50) {
+    console.log('Você foi multado!! Velocidade limite 50km, vicê passou a ' + velocidade + 'km')
+}else{
+    console.log(`Você passou a ${velocidade}`)
+}
---
+++ Javascript/3_estrutura/ex6.js
@@ -0,0 +1,7 @@
+let carteira = true
+let idade = 20
+
+
+if (carteira && idade >= 18){
+    console.log('Você pode dirigir')
+}else if (!carteira && )
