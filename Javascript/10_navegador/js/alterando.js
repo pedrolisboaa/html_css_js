82a3e6a6a0f810730c3c40349536e105a4110b00
pedrolisboaa <phlisboa2000@gmail.com>
2023-08-03 21:39:57 -0300

    subundo

--- Javascript/10_navegador/js/alterando.js
+++ Javascript/10_navegador/js/alterando.js
@@ -1,6 +1,5 @@
 // Inserindo com insertBefore
 let novoElemento = document.createElement('p')
-
 let texto = document.createTextNode('Esse texto é um teste para inserir.')
 novoElemento.appendChild(texto)
 
@@ -15,4 +14,38 @@ let novoNome = document.getElementsByClassName('nome_completo')
 let paiDoNome = novoElemento.parentNode
 
 paiDoNome.appendChild(novoNomeMae)
+
+// Outro exemplo de appendChild
+let divPai = document.getElementById('divAppend')
+let novaTagTitulo = document.createElement('h2')
+novaTagTitulo.textContent = 'Esse é o novo título que vai ser inserido com o append'
+divPai.appendChild(novaTagTitulo)
+
+
+// Replace
+
+let aquiEPai = document.getElementById('footer')
+let aquiEOFilho = document.querySelector('footer p')
+let novoFilho = document.createElement('h1')
+novoFilho.textContent = 'Aqui é o novo footer que foi realizado a mudança '
+aquiEPai.replaceChild(novoFilho, aquiEOFilho)
+
+// Inserido texto
+let elementoSemTexto = document.getElementById('header')
+let novoTextoDoHeader = document.createTextNode('Aqui é um texto criado pelo createTextNode')
+elementoSemTexto.appendChild(novoTextoDoHeader)
+
+
+// Criando varioas elemento
+const array = ['maçã', 'banana', 'abacaxi', 'uva', 'pera']
+let selecionandoDiv = document.getElementById('futura_lista')
+let ulDaLista = document.createElement('ol')
+selecionandoDiv.appendChild(ulDaLista)
+
+for(let i = 0; i < array.length; i++){
+    let itemLista = document.createElement('li')
+    itemLista.textContent = `${array[i]}`
+    ulDaLista.appendChild(itemLista)
+}
+
 
